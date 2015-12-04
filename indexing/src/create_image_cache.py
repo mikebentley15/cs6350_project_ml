0da1cedd95741ee48d9fac994fe5a658ef917379
Michael Bentley <michaelb@cs.utah.edu>
2015-12-03 22:00:49 -0700

    create_image_cache: finish cleaning up

--- indexing/src/create_image_cache.py
+++ indexing/src/create_image_cache.py
@@ -1,32 +1,50 @@
-import os
-import sys
-import timeit
+'''
+Creates an image cache.  Call this script with --help for more info
+'''
+
 import imagefuncs
 import ldsImporter
 import utils
 
-import argparse
-
-import numpy
-import csv
-
-import theano
-import theano.tensor as T
-from theano.tensor.signal import downsample
-from theano.tensor.nnet import conv
 import cv2
 
-def main(filepath, outdir):
-    with open(filepath, 'r') as csvfile:
+import argparse
+import csv
+import os
+import sys
+
+def parseArgs(arguments):
+    '''
+    Parses the arguments for this script.  Returns a parser arguments object.
+    '''
+    parser = argparse.ArgumentParser(description='''
+        Reads data from a tsv file, including image path, and generates an
+        image cache for the 'sex' field of the images.   The images output will
+        be all the same size, which is 40 x 68.  The names of the images
+        in the cache directory will be <input-image-name>-<line-number>.png.
+        ''')
+    parser.add_argument('filepath', help='tsv file containing labeled data')
+    parser.add_argument('outdir', help='Where to generate the image cache')
+    return parser.parse_args(args=arguments)
+
+def main(arguments):
+    '''
+    Main entry point.  Call with the --help option for more information
+    on usage.
+    '''
+    args = parseArgs(arguments)
+    with open(args.filepath, 'r') as csvfile:
         reader = csv.DictReader(csvfile, dialect='excel-tab')
         lines = [line for line in reader]
     imagepaths = set(line['imagePath'] for line in lines)
     print len(imagepaths), 'images'
-    utils.mkdir_p(outdir)
-    newsize = (52, 68)
+    utils.mkdir_p(args.outdir)
+    newsize = (40, 68)
     for imagepath in imagepaths:
         image = cv2.imread(imagepath)
-        relevantLines = [line for line in lines if line['imagePath'] == imagepath]
+        relevantLines = [
+            line for line in lines if line['imagePath'] == imagepath
+            ]
         for line in relevantLines:
             boundbox = ldsImporter.BoundingBox(
                 int(line['sex-top']),
@@ -38,16 +56,12 @@ def main(filepath, outdir):
             newimage = imagefuncs.mirrorPadImage(newimage, newsize)
             imagenameComponents = os.path.splitext(os.path.basename(imagepath))
             outpath = os.path.join(
-                outdir,
+                args.outdir,
                 imagenameComponents[0] + '-' + line['line'] + '.png'
                 )
             cv2.imwrite(outpath, newimage)
         print imagepath
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('filepath')
-    parser.add_argument('outdir')
-    args = parser.parse_args()
-    main(args.filepath, args.outdir)
+    main(sys.argv[1:])
 
