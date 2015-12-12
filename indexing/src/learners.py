5cb354b7359a6216e3d79bdb0a85db92be166398
Jackson Pontsler <jacksonpontsler@gmail.com>
2015-12-12 15:59:46 -0700

    Updated to include SVM Loss

--- indexing/src/learners.py
+++ indexing/src/learners.py
@@ -109,7 +109,7 @@ class Sgd(object):
                 # This is where training actually occurs
                 trainingFunction(minibatchIndex)
             # Print a period at every 10% done
-            if epoch % (epochs / 10) == 0:
+            if epoch % max(1, (epochs / 10)) == 0:
                 sys.stdout.write('.')
                 sys.stdout.flush()
 
@@ -124,6 +124,19 @@ def perceptron_loss(x, y, w, b):
     @return The perceptron loss function with x, y, w, and b inside
     '''
     return T.sum(T.maximum(0, - y * (x.dot(w) + b).transpose()))
+    
+    
+def SVM_loss(x, y, w, b, C):
+    '''
+    Returns a perceptron loss function
+
+    @param x  Features (theano matrix)
+    @param y  Label (theano ivector)
+    @param w  Weight matrix (theano matrix)
+    @param b  Bias vector (theano vector)
+    @return The perceptron loss function with x, y, w, and b inside
+    '''
+    return T.sum(T.maximum(0, 1 - y * (x.dot(w) + b).transpose()))    
 
 class Perceptron(Sgd):
     '''
@@ -138,6 +151,60 @@ class Perceptron(Sgd):
         @param r   Learning rate
         '''
         super(self.__class__, self).__init__(perceptron_loss, dim, 1, r)
+
+    def predict(self, xdata):
+        '''
+        Takes an itterable containing the data as a 2D array.
+
+        Returns a list of labels of -1 or +1.  There is a small probability of
+        getting a label of 0 which happens if the point is exactly on the
+        hyperplane.
+
+        @param xdata Feature list to classify
+        @return list of labels
+        '''
+        xdata_share = theano.shared(
+            np.asarray(xdata, dtype=theano.config.floatX),
+            borrow=True
+            )
+        answers = T.sgn(xdata_share.dot(self.w) + self.b).eval()
+        # The answers array is shaped as a (n,1) 2D array.  We want to reshape
+        # to a 1D array.
+        return answers.reshape((xdata_share.get_value(borrow=True).shape[0],))
+
+class SVM(Sgd):
+    '''
+    Implements the SVM algorithm.
+
+    Note: a bias term is already present.
+    '''
+
+    def __init__(self, dim, C, r):
+        '''
+        @param dim Number of dimensions in the input
+        @param r   Learning rate
+        @param C   First Step
+        '''
+        self.C = C
+        my_loss = lambda x, y, w, b: SVM_loss(x, y, w, b, self.C)
+        super(self.__class__, self).__init__(my_loss, dim, 1, r)
+        self.r0 = r
+        self.r = theano.shared(
+            value=np.array([r],
+                dtype=theano.config.floatX
+            ),
+            name='r',
+            borrow=True
+            )
+        self.t = theano.shared(
+            value=np.zeros(1,
+                dtype=np.int32
+            ),
+            name='t',
+            borrow=True
+            )
+        self.updates.append((self.r, self.r0/(1+self.r0*self.t/C)))
+        self.updates.append((self.t, self.t+1))
 
     def predict(self, xdata):
         '''
@@ -245,13 +312,13 @@ def main(arguments):
     print 'epochs fixed:        epochs =', epochs
     print 'batch size fixed:     batch =', batchSize
 
-    basedir = '/home/bentley/classes/cs6350_machine_learning/hw/handin/hw02/data'
+    basedir = '/home/pontsler/Documents'
 
     sets = [
         # Name         training path       testing path
         #('sanity   ', 'sanityCheck-train.dat', 'sanityCheck-train.dat'),
-        ('train0.10', 'data0/train0.10', 'data0/test0.10'),
-        ('train0.20', 'data0/train0.20', 'data0/test0.20'),
+        #('train0.10', 'data0/train0.10', 'data0/test0.10'),
+        #('train0.20', 'data0/train0.20', 'data0/test0.20'),
         ('train1.10', 'data1/train1.10', 'data1/test1.10'),
         ]
 
@@ -272,7 +339,8 @@ def testPerceptron(name, trainExamples, testExamples, r = 0.2, epochs = 10, batc
     featuresList = [x.features for x in trainExamples]
     labels = [x.label for x in trainExamples]
     #p = Perceptron(len(featuresList[0]), r)
-    p = AveragedPerceptron(len(featuresList[0]), r)
+    #p = AveragedPerceptron(len(featuresList[0]), r)
+    p = SVM(len(featuresList[0]),20,.01)
 
     print 'training ' + name + ' ',
     sys.stdout.flush()
@@ -292,6 +360,9 @@ def testPerceptron(name, trainExamples, testExamples, r = 0.2, epochs = 10, batc
     predictions = p.predict(testFeatures)
     accuracy = np.sum(testLabels == predictions) / float(len(testLabels))
     print name, '  test accuracy:   ', accuracy
+    
+
+    
 
 if __name__ == '__main__':
     main(sys.argv[1:])
